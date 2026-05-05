593d6beccce6701b692bbd5e4dad1d9c49822f0c
HugoP <leogitrepo@gmail.com>
2026-05-05 18:35:25 -0500

    feat[JS]: se añadieron variables para manipular objetos del DOM (contenedor, btnAgregarItem, btnGenerarPaleta, arreglo de colores

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*ELEMENTOS DEL DOM*/
+
+//Elemento Contenedor del DOM
+const divContenedor = document.querySelector('.contenedor')
+//Elemento Agregar Color del DOM
+const btnAgregarItem = document.getElementById('botonAgregarColor')
+//Elemento Generar Paleta en DOM
+const btnGenerarPaleta = document.getElementById('botonGenerarPaleta')
+//Arreglo de colores para paletas 
+const paletaUno = ["#FFC2D1","#FFE5B4","#FFF4BD","#C2F0D5","#BDEFF4","#D1E9FF","#E0D7FF","#F4D7F4","#E6E9ED"]
+const paletaDos = ["#0077B6","#00B4D8","#90E0EF","#CAF0F8","#F4D35E","#E9C46A","#F4A261","#D4A373","#FAF9F6"]
+
+
+
